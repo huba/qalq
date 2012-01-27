e207891f476f9abeb7d9eb0806b6c41767979d78
Huba Nagy <12huba@gmail.com>
2012-01-28 01:11:28 +1300

    Lots of bugs, there is still work to be done.

---
+++ calc.h
@@ -0,0 +1,99 @@
+/**************************************************************************
+** calc.h
+** This file is part of the "qalq" project,
+**   and is released under the MIT Licence.
+** Created by Huba Nagy on %DATE%.
+** Copyright (c) %YEAR% Huba Z. Nagy
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+** OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+** HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+** WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+** OTHER DEALINGS IN THE SOFTWARE.
+**************************************************************************/
+
+#ifndef CALC_H
+#define CALC_H
+
+#include <QWidget>
+#include <iostream>
+#include <sstream>
+#include <qmath.h>
+
+namespace Ui {
+class Calc;
+}
+
+class Calc : public QWidget
+{
+    Q_OBJECT
+    
+public:
+    explicit Calc(QWidget *parent = 0);
+    ~Calc();
+    
+private slots:
+
+    void on_clear_clicked();
+
+    void on_times_clicked();
+
+    void on_div_clicked();
+
+    void on_minus_clicked();
+
+    void on_seven_clicked();
+
+    void on_eight_clicked();
+
+    void on_nine_clicked();
+
+    void on_plus_clicked();
+
+    void on_four_clicked();
+
+    void on_five_clicked();
+
+    void on_six_clicked();
+
+    void on_one_clicked();
+
+    void on_two_clicked();
+
+    void on_three_clicked();
+
+    void on_zero_clicked();
+
+    void on_equals_clicked();
+
+    void on_point_clicked();
+
+    void on_sqrt_clicked();
+
+private:
+    Ui::Calc *ui;
+
+    void loadLicence();
+    void notImplemented();
+    void number(QString s);
+
+    int donone;
+    int add;
+    int subtract;
+    int multiply;
+    int divide;
+    bool decimal;
+    bool multiple;
+    bool multiplediff;
+    bool first;
+
+    int operation;
+
+    double number1;
+    double number2;
+};
+
+#endif // CALC_H
---
+++ main.cpp
@@ -0,0 +1,28 @@
+/**************************************************************************
+** main.cpp
+** This file is part of the "qalq" project,
+**   and is released under the MIT Licence.
+** Created by Huba Nagy on %DATE%.
+** Copyright (c) %YEAR% Huba Z. Nagy
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+** EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+** OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+** NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+** HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+** WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+** OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+** OTHER DEALINGS IN THE SOFTWARE.
+**************************************************************************/
+
+#include <QtGui/QApplication>
+#include "calc.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Calc w;
+    w.show();
+    
+    return a.exec();
+}
